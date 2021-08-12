eefd91c2fabd6bffe43009a5976671579c7734ce
fengyafei <fengyafei@bjca.org.cn>
2021-08-12 20:16:59 +0800

    添加jenkins学习心得

--- algorithm/src/main/java/com/newcoder/algorithm/config/DataSourceConfig.java
+++ algorithm/src/main/java/com/newcoder/algorithm/config/DataSourceConfig.java
@@ -1,14 +1,7 @@
 package com.newcoder.algorithm.config;
 
-import javax.sql.DataSource;
-
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.shardingsphere.encrypt.api.EncryptColumnRuleConfiguration;
-import org.apache.shardingsphere.encrypt.api.EncryptRuleConfiguration;
-import org.apache.shardingsphere.encrypt.api.EncryptTableRuleConfiguration;
-import org.apache.shardingsphere.encrypt.api.EncryptorRuleConfiguration;
-import org.apache.shardingsphere.shardingjdbc.api.EncryptDataSourceFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
@@ -20,10 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
+import javax.sql.DataSource;
 
 /**
  * 数据源配置
@@ -45,8 +35,8 @@ public class DataSourceConfig {
   @Bean(name = "dbDataSource")
   @ConfigurationProperties(prefix = "spring.datasource.hikari.db")
   @Primary
-  public DataSource buildDataSource() throws SQLException {
-    return EncryptDataSourceFactory.createDataSource(DataSourceBuilder.create().build(), getEncryptRuleConfiguration(), new Properties());
+  public DataSource buildDataSource() {
+    return DataSourceBuilder.create().build();
   }
 
   /**
@@ -84,49 +74,6 @@ public class DataSourceConfig {
   public SqlSessionTemplate buildSqlSessionTemplate(@Qualifier("dbSqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
     return new SqlSessionTemplate(sqlSessionFactory);
   }
-
-  private EncryptRuleConfiguration getEncryptRuleConfiguration() {
-    Properties props = new Properties();
-
-    //自带aes算法需要
-    props.setProperty("aes.key.value", "aeskey");
-    EncryptorRuleConfiguration encryptorConfig = new EncryptorRuleConfiguration("AES", props);
-
-    //自定义算法
-    //props.setProperty("qb.finance.aes.key.value", aeskey);
-    //EncryptorRuleConfiguration encryptorConfig = new EncryptorRuleConfiguration("QB-FINANCE-AES", props);
-
-    EncryptRuleConfiguration encryptRuleConfig = new EncryptRuleConfiguration();
-    encryptRuleConfig.getEncryptors().put("aes", encryptorConfig);
-
-    //START: card_info 表的脱敏配置
-    {
-      EncryptColumnRuleConfiguration columnConfig1 = new EncryptColumnRuleConfiguration("", "name", "", "aes");
-      EncryptColumnRuleConfiguration columnConfig2 = new EncryptColumnRuleConfiguration("", "id_no", "", "aes");
-      EncryptColumnRuleConfiguration columnConfig3 = new EncryptColumnRuleConfiguration("", "finshell_card_no", "", "aes");
-      Map<String, EncryptColumnRuleConfiguration> columnConfigMaps = new HashMap<>();
-      columnConfigMaps.put("name", columnConfig1);
-      columnConfigMaps.put("id_no", columnConfig2);
-      columnConfigMaps.put("finshell_card_no", columnConfig3);
-      EncryptTableRuleConfiguration tableConfig = new EncryptTableRuleConfiguration(columnConfigMaps);
-      encryptRuleConfig.getTables().put("card_info", tableConfig);
-    }
-    //END: card_info 表的脱敏配置
-
-    //START: pay_order 表的脱敏配置
-    {
-      EncryptColumnRuleConfiguration columnConfig1 = new EncryptColumnRuleConfiguration("", "card_no", "", "aes");
-      Map<String, EncryptColumnRuleConfiguration> columnConfigMaps = new HashMap<>();
-      columnConfigMaps.put("card_no", columnConfig1);
-      EncryptTableRuleConfiguration tableConfig = new EncryptTableRuleConfiguration(columnConfigMaps);
-      encryptRuleConfig.getTables().put("pay_order", tableConfig);
-    }
-
-    log.info("脱敏配置构建完成:{} ", encryptRuleConfig);
-    return encryptRuleConfig;
-
-  }
-
 
 
 }
---
+++ algorithm/src/main/java/com/newcoder/algorithm/mac/MacHotKey.java
@@ -0,0 +1,10 @@
+package com.newcoder.algorithm.mac;
+
+public class MacHotKey {
+
+  StringBuffer stringBuffer = new StringBuffer();
+
+  public static void main(String[] args) {
+    //
+  }
+}
